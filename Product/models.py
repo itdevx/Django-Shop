95684c0069507052cdb4080c7aabd146b54bb91d
itdevx <testwebsite1244@gmail.com>
2024-07-06 18:45:21 +0330

    add api app

--- Product/models.py
+++ Product/models.py
@@ -17,7 +17,7 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=30)
     phone = models.CharField(max_length=20)
     email = models.EmailField()
-    
+
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
     
