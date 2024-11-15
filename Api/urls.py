b9973b2db9342ffa01eb092803dac22fcf8a4543
itdevx <testwebsite1244@gmail.com>
2024-11-15 09:23:09 +0330

    change

--- Api/urls.py
+++ Api/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from Api import views
+from rest_framework.routers import DefaultRouter
+
 
 
 app_name = 'api'
 
 
+router = DefaultRouter()
+
 urlpatterns = [
     path('product-list', views.ProductListApi.as_view(), name='product-list-api')
 ]
+
+
+router.register('products', views.ProductViewSet, basename='productsviewset')
+
+urlpatterns = router.urls
--- Api/views.py
+++ Api/views.py
@@ -3,10 +3,26 @@ from rest_framework import generics
 from Api.serializers import ProductSerializer
 from Product.models import Product
 from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+
 
 
 class ProductListApi(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [permissions.AllowAny]
+    
+
+class ProductViewSet(viewsets.ViewSet):
+    
+    def list(self, request):
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def retrive(self, request, pk):
+        ...
+
     
